0226dc72886d9e2e4d97af24c5b69d9f807c051b
obedthomas <obedthomas@hotmail.co.uk>
2019-08-09 17:16:08 +0100

    added dummy edit field on table row

--- client/src/views/Users/Users.jsx
+++ client/src/views/Users/Users.jsx
@@ -41,7 +41,13 @@ class Users extends Component {
       { dataField: 'company', text: 'Company', sort: true },
       { dataField: 'department', text: 'department', sort: true },
       { dataField: 'role', text: 'role', sort: true },
-      { dataField: 'edit', text: '', sort: false },
+      {
+        dataField: 'edit',
+        text: '',
+        isDummyField: true,
+        headerStyle: { width: '100px' },
+        sort: false,
+      },
     ]
 
     return (
@@ -81,55 +87,6 @@ class Users extends Component {
         </Container>
       </React.Fragment>
     )
-
-    // return (
-    //   <React.Fragment>
-    //     <DashboardHeader />
-    //     <Container className="mt--7" fluid>
-    //       <Row>
-    //         <div className="col">
-    // <Card className="shadow">
-    //   <CardHeader className="border-0">
-    //     <Row className="align-items-center">
-    //       <Col xs="8">
-    //         <h3 className="mb-0">All Users</h3>
-    //       </Col>
-    //       <Col className="text-right" xs="4">
-    //         <Link to="/admin/users/add-user" className="text-white">
-    //           <Button color="primary" size="sm">
-    //             Add
-    //           </Button>
-    //         </Link>
-    //       </Col>
-    //     </Row>
-    //   </CardHeader>
-    //             {/* add loading spinner here */}
-    //             <Table className="align-items-center table-flush" responsive>
-    //               <TableHead headers={headers} />
-    //               <tbody className="list">
-    //                 {users.users.map(user => (
-    //                   <tr key={user._id}>
-    //                     <th>{user.firstName}</th>
-    //                     <th>{user.lastName}</th>
-    //                     <td>
-    //                       {companies.map(company => {
-    //                         if (company._id === user.company)
-    //                           return company.name
-    //                       })}
-    //                     </td>
-    //                     <td>{user.department}</td>
-    //                     <td>{user.role}</td>
-    //                     <td>EDIT</td>
-    //                   </tr>
-    //                 ))}
-    //               </tbody>
-    //             </Table>
-    //           </Card>
-    //         </div>
-    //       </Row>
-    //     </Container>
-    //   </React.Fragment>
-    // )
   }
 }
 
